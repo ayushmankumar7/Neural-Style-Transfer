7c4212ff86827a348620c37e3c2728460bf9f5a1
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-02-01 22:38:59 +0530

    losses added

---
+++ losses.py
@@ -0,0 +1,27 @@
+
+
+def content_loss(base, combination):
+  return K.sum(K.square(combination - base))
+
+def gram_matrix(x):
+  features = K.batch_flatten(K.permute_dimensions(x, (2,0,1)))
+  gram = K.dot(features, K.transpose(features))
+  return gram
+
+def style_loss(style, combination):
+  S = gram_matrix(style)
+  C = gram_matrix(combination)
+  channels = 3
+  size = img_height * img_width
+  return K.sum(K.square(S-C)) / (4. * (channels ** 2) * (size ** 2))
+
+def total_variation_loss(x):
+  a = K.square(
+      x[:, :img_height - 1, :img_width - 1, :] - 
+      x[:, 1: , :img_width - 1 , :])
+  b = K.square(
+      x[:, :img_height - 1, :img_width - 1, :] - 
+      x[:, :img_height - 1, 1: , :]
+  )
+
+  return K.sum(K.pow(a+b, 1.25))
