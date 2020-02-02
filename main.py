95a6135b5b03c18a83d8bcab7ccd8237aaea66f7
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-02-02 21:46:51 +0530

    necessary changed made

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from process import preprocess_image, deprocess_image
 from losses import content_loss, gram_matrix, style_loss, total_variation_loss
 from scipy.optimize import fmin_l_bfgs_b
 import time
-from evaluator import Evaluator
+#from evaluator import Evaluator
 from keras.preprocessing.image import load_img, img_to_array
 from keras.applications import vgg19
 import argparse
@@ -76,6 +76,43 @@ loss.assign_add(total_variation_weight * total_variation_loss(combination_image,
 
 grads = K.gradients(loss, combination_image)[0]
 fetch_loss_and_grads = K.function([combination_image], [loss, grads])
+
+
+class Evaluator(object):
+  def __init__(self):
+    self.loss_value = None
+    self.grads_values = None
+  def loss(self, x):
+    assert self.loss_value is None
+    x = x.reshape((1, img_height, img_width, 3))
+    outs = fetch_loss_and_grads([x])
+    loss_value = outs[0]
+    grad_values = outs[1].flatten().astype('float64')
+    self.loss_value = loss_value
+    self.grad_values = grad_values
+    return self.loss_value
+  def grads(self, x):
+    assert self.loss_value is not None
+    grad_values = np.copy(self.grad_values)
+    self.loss_value = None
+    self.grad_values = None
+    return grad_values
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 evaluator = Evaluator()
 
